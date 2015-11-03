81c6276a67524d532c76a1045c7d0755121f269c
Seth Roberts <seth.b.roberts (at) gmail (dot) com>
2015-11-03 13:56:28 -0500

    all tests passing

--- shortly.js
+++ shortly.js
@@ -2,6 +2,8 @@ var express = require('express');
 var util = require('./lib/utility');
 var partials = require('express-partials');
 var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
 
 var db = require('./app/config');
 var Users = require('./app/collections/users');
@@ -12,10 +14,9 @@ var Click = require('./app/models/click');
 
 var app = express();
 
-/*
-app.use(express.cookieParser());
-app.use(express.session({secret: '1234567890QWERTY'}));
-*/
+
+app.use(cookieParser()); //**
+app.use(session({secret: '1234567890QWERTY'}));
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
@@ -29,53 +30,70 @@ app.use(express.static(__dirname + '/public'));
 
 app.get('/', 
 function(req, res) {
-  res.render('index');
+  console.log(req.session.loggedIn, '*********');
+  if (req.session.loggedIn) {
+    res.render('index');
+  } else {
+    res.redirect('login');
+  }
 });
 
 app.get('/create', 
 function(req, res) {
-  res.render('index');
+  if (req.session.loggedIn) {
+    res.render('index');
+  } else {
+    res.redirect('login');
+  }
 });
 
 app.get('/links', 
 function(req, res) {
-  Links.reset().fetch().then(function(links) {
-    res.send(200, links.models);
-  });
+  if (req.session.loggedIn) {
+    Links.reset().fetch().then(function(links) {
+      res.send(200, links.models);
+    });
+  } else {
+    res.redirect('login');
+  }
 });
 
 app.post('/links', 
 function(req, res) {
-  var uri = req.body.url;
+  if (req.session.loggedIn) {
+    var uri = req.body.url;
 
-  if (!util.isValidUrl(uri)) {
-    console.log('Not a valid url: ', uri);
-    return res.send(404);
-  }
-
-  new Link({ url: uri }).fetch().then(function(found) {
-    if (found) {
-      res.send(200, found.attributes);
-    } else {
-      util.getUrlTitle(uri, function(err, title) {
-        if (err) {
-          console.log('Error reading URL heading: ', err);
-          return res.send(404);
-        }
-
-        var link = new Link({
-          url: uri,
-          title: title,
-          base_url: req.headers.origin
-        });
-
-        link.save().then(function(newLink) {
-          Links.add(newLink);
-          res.send(200, newLink);
-        });
-      });
+    if (!util.isValidUrl(uri)) {
+      console.log('Not a valid url: ', uri);
+      return res.send(404);
     }
-  });
+
+    new Link({ url: uri }).fetch().then(function(found) {
+      if (found) {
+        res.send(200, found.attributes);
+      } else {
+        util.getUrlTitle(uri, function(err, title) {
+          if (err) {
+            console.log('Error reading URL heading: ', err);
+            return res.send(404);
+          }
+
+          var link = new Link({
+            url: uri,
+            title: title,
+            base_url: req.headers.origin
+          });
+
+          link.save().then(function(newLink) {
+            Links.add(newLink);
+            res.send(200, newLink);
+          });
+        });
+      }
+    });
+  } else {
+    res.redirect('login');
+  }
 });
 
 /************************************************************/
@@ -84,6 +102,7 @@ function(req, res) {
 
 app.get('/logout', function(req, res) {
   //todo: more logic here to stop session 
+  req.session.loggedIn = false;
   res.render('login');
 });
 
@@ -92,7 +111,7 @@ app.get('/login', function(req, res) {
 });
 
 app.post('/login', function(req, res) {
-  console.log(req.body);
+  //console.log(req.body);
   new User({ 
     username: req.body.username, 
     password: req.body.password 
@@ -100,8 +119,9 @@ app.post('/login', function(req, res) {
     if (!user) {
       res.redirect('/login');
     } else {
-      console.log(user);
-      //req.session.loggedIn = true;
+      //console.log(user);
+      req.session.loggedIn = true;
+      console.log(user, req.session.loggedIn);
       res.redirect('/');
     }
   });
@@ -113,7 +133,7 @@ app.get('/signup', function(req, res) {
 
 app.post('/signup', function(req, res) {
   console.log(req.body.username);
-  
+
   //succesful creation of new user row for User table
   var user = new User({
     username: req.body.username,
@@ -132,7 +152,7 @@ app.post('/signup', function(req, res) {
 /************************************************************/
 
 app.get('/*', function(req, res) {
-  console.log(req.params);
+  //console.log(req.params);
   new Link({ code: req.params[0] }).fetch().then(function(link) {
     if (!link) {
       res.redirect('/');
